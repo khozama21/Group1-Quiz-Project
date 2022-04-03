638aaabf3f568999f392938eb85f5c613b8d92ea
RubaAlmasri <100359514+RubaAlmasri@users.noreply.github.com>
2022-04-03 22:45:21 +0300

    main

--- Ruba/mark.js
+++ Ruba/mark.js
@@ -7,13 +7,13 @@ let result=mark1 + mark2 + mark3
 // mark=10
 if (result>=10){
     document.getElementById('msg1').style.display="block"
-    document.getElementById('mark2').innerHTML+=mark + ' / 20'
+    document.getElementById('mark2').innerHTML+=result + ' / 20'
     document.getElementById('mark2').style.color='green'
     document.getElementById('markimg').src='images/pass.png'
 }
 else{
     document.getElementById('msg2').style.display="block"
-    document.getElementById('mark2').innerHTML+=mark + ' / 20'
+    document.getElementById('mark2').innerHTML+=result + ' / 20'
     document.getElementById('mark2').style.color='red'
     document.getElementById('markimg').src='images/fail.png'
 }
