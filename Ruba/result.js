733a233ae7e66baa79de5b4987378ef38d7ab9d5
Abdullah_assoli <abdullahassoli@gmail.com>
2022-04-04 09:47:47 +0200

    updating

--- Ruba/result.js
+++ Ruba/result.js
@@ -1,6 +1,10 @@
 let a = (JSON.parse(window.localStorage.getItem("allWrong")))
 
+let email=localStorage.getItem("email");
+let pass=localStorage.getItem("pw");
+let name1=localStorage.getItem("name")
+localStorage.clear();
 
-for(let i=1; i<a.length; i++){
-console.log(a[i])
-}
+localStorage.setItem("email",email)
+localStorage.setItem("pw",pass)
+localStorage.setItem("name",name1)
